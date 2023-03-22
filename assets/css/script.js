6b012c0a6135581355538e8f369c05b2378c2b41
Pablo <thepablodead18@gmail.com>
2023-03-21 22:37:54 -0300

    portfolio

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,21 +1,21 @@
 function seleccionar(link) {
-    var opciones = document.querySelector("links a");
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
+    var opciones = document.querySelector('links');
+    opciones[0].className = '';
+    opciones[1].className = '';
+    opciones[2].className = '';
+    opciones[3].className = '';
+    opciones[4].className = '';
 
-    var x = document.getElementById("navbar");
-    x.className = "";
+    var x = document.getElementById('navbar');
+    x.className = '';
 }
 
 function responsiveMenu() {
-    var x = document.getElementById("navbar");
-    if (x.className === "") {
-        x.className = "responsive";
+    var x = document.querySelector('icono-nav');
+    if (x.className === '') {
+        x.className ='responsive';
     } else {
-        x.className = "";
+        x.className = '';
     }
 }
 
@@ -24,14 +24,14 @@ window.onscroll = function() {
 }
 
 function efectoHabilidades() {
-    var skills = document.getElementById("skills");
+    var skills = document.getElementById('skills');
     var distancia_skills = window.innerHeight = skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
-        document.getElementById("htmlcss").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bootstrap").classList.add("barra-progreso3");
-        document.getElementById("comunicacion").classList.add("barra-progreso4");
-        document.getElementById("equipo").classList.add("barra-progreso5");
-        document.getElementById("creatividad").classList.add("barra-progreso5");
+        document.getElementById('htmlcss').classList.add('barra-progreso1');
+        document.getElementById('js').classList.add('barra-progreso2');
+        document.getElementById('bootstrap').classList.add('barra-progreso3');
+        document.getElementById('comunicacion').classList.add('barra-progreso4');
+        document.getElementById('equipo').classList.add('barra-progreso5');
+        document.getElementById('creatividad').classList.add('barra-progreso5');
     }
 }
